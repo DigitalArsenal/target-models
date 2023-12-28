163024f712d98536963ad3d8bea4b11002b058cb
TJ Koury <tj@digitalarsenal.io>
2023-12-28 11:44:54 -0500

    updates

--- satnogs.js
+++ satnogs.js
@@ -1,36 +1,55 @@
 import fetch from "node-fetch";
 import { writeFileSync } from "fs";
+import satnogs_sites from "./data/satnogs.stations.json" assert {type: "json"};
 
-async function fetchAllStations() {
-    let page = 1;
-    let hasMorePages = true;
-    const allStations = [];
+async function fetchCurrentStations() {
+    let response = await fetch("https://network.satnogs.org/stations_all/", {
+        "headers": {
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "no-cache",
+            "pragma": "no-cache",
+            "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Brave\";v=\"120\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Linux\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "sec-gpc": "1",
+            "x-requested-with": "XMLHttpRequest",
+            "cookie": "csrftoken=1yOeycsf3r50w7NJ2v4V3MIayCWXRqDMahNXt7CSEd8g6fx5O5ALA6XHkXXkmMF4; sessionid=54qc9n91dapbx7xttoixm356rawvdkty",
+            "Referer": "https://network.satnogs.org/",
+            "Referrer-Policy": "same-origin"
+        },
+        "body": null,
+        "method": "GET"
+    });
 
-    while (hasMorePages) {
-        console.log(`retrieving page ${page}`);
-        const response = await fetch(`https://network.satnogs.org/api/stations/?format=json&page=${page}`);
-
-        if (!response.ok) {
-            const errorDetail = await response.json();
-            if (errorDetail.detail === 'Invalid page.') {
-                hasMorePages = false;
-            } else {
-                console.error('Error fetching data:', errorDetail);
-            }
-            break;
-        }
-
-        const data = await response.json();
-        allStations.push(...data);
-        page++;
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    return allStations;
+    return await response.json();
 }
 
-fetchAllStations().then(allStations => {
-    console.log('Total Stations:', allStations.length);
-    writeFileSync("./data/satnogs.stations.js", JSON.stringify(allStations, null, 4));
-}).catch(error => {
+async function updateStations() {
+    const existingStationIds = new Set(satnogs_sites.map(station => station.id));
+    const currentStations = await fetchCurrentStations();
+    let stationsToUpdate = [];
+
+    for (const station of currentStations) {
+        if (!existingStationIds.has(station.id)) {
+            console.log(station.id);
+            stationsToUpdate.push(station);
+        }
+    }
+
+    if (stationsToUpdate.length > 0) {
+        satnogs_sites.push(...stationsToUpdate);
+        writeFileSync("./data/satnogs.stations.json", JSON.stringify(satnogs_sites, null, 4));
+    }
+}
+
+updateStations().catch(error => {
     console.error('An error occurred:', error);
 });
